566d17efecd28d6c22aaeeed45a80726bd17486a
Anthony Vu <70167429+vua11@users.noreply.github.com>
2022-01-24 12:49:01 -0500

    Update 8-10.sql

--- 8-10.sql
+++ 8-10.sql
@@ -1,1 +1,7 @@
-
+SELECT teacher.name,
+CASE
+WHEN dept.id = 1 THEN 'Sci'
+WHEN dept.id = 2 THEN 'Sci'
+WHEN dept.id = 3 THEN 'Art'
+ELSE 'None' END
+FROM teacher LEFT JOIN dept ON (dept.id=teacher.dept)
