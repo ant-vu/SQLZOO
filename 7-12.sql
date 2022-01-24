f98719d63d49e233400abbf002d726d877d00c39
Anthony Vu <70167429+vua11@users.noreply.github.com>
2022-01-24 12:26:54 -0500

    Update 7-12.sql

--- 7-12.sql
+++ 7-12.sql
@@ -1,1 +1,1 @@
-
+SELECT title, name FROM movie, casting, actor WHERE movieid=movie.id AND actorid=actor.id AND ord=1 AND movieid IN (SELECT movieid FROM casting, actor WHERE actorid=actor.id AND name='Julie Andrews')
