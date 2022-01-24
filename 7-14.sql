fce0992983220167a5441f0e338548f97df327dd
Anthony Vu <70167429+vua11@users.noreply.github.com>
2022-01-24 12:29:50 -0500

    Update 7-14.sql

--- 7-14.sql
+++ 7-14.sql
@@ -1,1 +1,1 @@
-
+SELECT title, COUNT(actorid) FROM (movie JOIN casting ON movieid = movie.id) JOIN actor ON actor.id = actorid WHERE yr = 1978 GROUP BY title ORDER BY COUNT(actorid) DESC, title, movieid
