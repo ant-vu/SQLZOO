741c32c387a0dd86e40bd1ccd59246ca3bc3b791
Anthony Vu <70167429+vua11@users.noreply.github.com>
2022-01-24 11:01:30 -0500

    Update 4-9.sql

--- 4-9.sql
+++ 4-9.sql
@@ -1,1 +1,1 @@
-
+SELECT name, continent, population FROM world WHERE continent IN (SELECT continent FROM world x WHERE 25000000 >= (SELECT MAX(population) FROM world y WHERE x.continent = y.continent))
