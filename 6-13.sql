8abcfc9f05faefa38c70617a88967aeeaab8f087
Anthony Vu <70167429+vua11@users.noreply.github.com>
2022-01-24 12:03:33 -0500

    Update 6-13.sql

--- 6-13.sql
+++ 6-13.sql
@@ -1,1 +1,1 @@
-
+SELECT game.mdate, game.team1, SUM(CASE WHEN goal.teamid = game.team1 THEN 1 ELSE 0 END) AS score1, game.team2, SUM(CASE WHEN goal.teamid = game.team2 THEN 1 ELSE 0 END) AS score2 FROM game LEFT JOIN goal ON matchid = id GROUP BY game.id,game.mdate, game.team1, game.team2 ORDER BY mdate,matchid,team1,team2
